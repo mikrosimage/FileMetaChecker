267b92ea917f61d7e90169f141fc844e66452a7a
Valentin Noel <van@mikrosimage.eu>
2013-05-06 16:27:13 +0200

    add groups unit test issue #5

--- libraries/extractor/tests/nodeSpecification/nodeSpecification.cpp
+++ libraries/extractor/tests/nodeSpecification/nodeSpecification.cpp
@@ -998,6 +998,42 @@ BOOST_AUTO_TEST_CASE( nodeSpecification_endiannessTestFile )
 	}
 }
 
+BOOST_AUTO_TEST_CASE( nodeSpecification_groupTestFile )
+{
+	{
+		const std::string nodename = "groupTest";
+		bpt::ptree node;
+		node.put( "id",  nodename );
+		node.put( "label", "Group Test" );
+		
+		std::ofstream stream( testFile.c_str(), std::ofstream::out );
+		stream << "ASCII" << (uint8) 0x00 << (uint8) 0x10;
+		stream.close();
+
+		const std::string nodename1 = "groupNode1";
+		bpt::ptree subnode1;
+		subnode1.put( "id",  nodename1 );
+		subnode1.put( "label", "Group Node 1" );
+		subnode1.put( "ascii", "ASCII" );
+
+		const std::string nodename2 = "groupNode2";
+		bpt::ptree subnode2;
+		subnode2.put( "id",  nodename2 );
+		subnode2.put( "label", "Group Node 2" );
+		subnode2.put( "type", "uint16" );
+
+		node.add_child( "group.", subnode1 );
+		node.add_child( "group.", subnode2 );
+
+		bpt::ptree report = generateReportTree( node );
+		CHECK_NODE_VALID ( nodename + "." + nodename1 );
+		CHECK_VALUE_EQUAL( nodename + "." + nodename1, "ASCII" );
+		CHECK_NODE_VALID ( nodename + "." + nodename2 );
+		CHECK_VALUE_EQUAL( nodename + "." + nodename2, "16" );
+		CHECK_NODE_VALID ( nodename );
+	}
+}
+
 BOOST_AUTO_TEST_CASE( nodeSpecification_optionalTestFile )
 {
 	{
