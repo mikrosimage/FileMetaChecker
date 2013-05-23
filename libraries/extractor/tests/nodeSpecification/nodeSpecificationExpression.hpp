5761cd2cf405a4a52a4cb5ac2832383e8fee1be6
Valentin Noel <van@mikrosimage.eu>
2013-05-23 12:00:45 +0200

    add unit tests for 'required' expressions issue #20

--- libraries/extractor/tests/nodeSpecification/nodeSpecificationExpression.hpp
+++ libraries/extractor/tests/nodeSpecification/nodeSpecificationExpression.hpp
@@ -1,12 +1,12 @@
 BOOST_AUTO_TEST_SUITE( nodeSpecification_tests_expression )
 
-BOOST_AUTO_TEST_CASE( nodeSpecification_expressionTestFile )
+BOOST_AUTO_TEST_CASE( nodeSpecification_countTestFile )
 {
 	{
-		const std::string nodename = "expressionTest";
+		const std::string nodename = "countTest";
 		bpt::ptree tree;
 		tree.put( "id",  nodename );
-		tree.put( "label", "Expression Test" );
+		tree.put( "label", "Count Test" );
 
 		const std::string subnodename1 = "value1";
 		bpt::ptree subnode1;
@@ -41,6 +41,86 @@ BOOST_AUTO_TEST_CASE( nodeSpecification_expressionTestFile )
 		CHECK_NODE_VALID ( nodename + "." + subnodename2 );
 		CHECK_NODE_VALID ( nodename + "." + subnodename3 );
 		CHECK_NODE( nodename + "." + subnodename3, "<xmlattr>.size", "4" );
+	}
+}
+
+BOOST_AUTO_TEST_CASE( nodeSpecification_requiredTestFile )
+{
+	{
+		const std::string nodename = "requiredTest";
+		bpt::ptree tree;
+		tree.put( "id",  nodename );
+		tree.put( "label", "Required Test" );
+
+		const std::string subnodename1 = "value1";
+		bpt::ptree subnode1;
+		subnode1.put( "id",  subnodename1 );
+		subnode1.put( "label", "Value 1" );
+		subnode1.put( "type", "uint8" );
+
+		const std::string subnodename2 = "value2";
+		bpt::ptree subnode2;
+		subnode2.put( "id",  subnodename2 );
+		subnode2.put( "label", "Value 2" );
+		subnode2.put( "ascii", "ok" );
+		subnode2.put( "required", "value1 == 1" );
+
+		tree.add_child( "group.", subnode1 );
+		tree.add_child( "group.", subnode2 );
+
+		{
+			std::ofstream stream( testFile.c_str(), std::ofstream::out );
+			stream << (uint8) 1 << "ok";
+			stream.close();
+
+			bpt::ptree report = generateReportTree( tree );
+			CHECK_NODE_VALID ( nodename + "." + subnodename1 );
+			CHECK_VALUE_EQUAL( nodename + "." + subnodename1, "1" );
+			CHECK_NODE_VALID ( nodename + "." + subnodename2 );
+			CHECK_VALUE_EQUAL( nodename + "." + subnodename2, "ok" );
+			CHECK_NODE_VALID ( nodename );
+		}
+		{
+			std::ofstream stream( testFile.c_str(), std::ofstream::out );
+			stream << (uint8) 2 << "ok";
+			stream.close();
+
+			bpt::ptree report = generateReportTree( tree );
+			CHECK_NODE_VALID ( nodename + "." + subnodename1 );
+			CHECK_VALUE_EQUAL( nodename + "." + subnodename1, "2" );
+			BOOST_CHECK_THROW( CHECK_NODE_VALID ( nodename + "." + subnodename2 ), std::runtime_error );
+			CHECK_NODE_VALID ( nodename );
+		}
+	}
+	{
+		const std::string nodename = "requiredTest";
+		bpt::ptree tree;
+		tree.put( "id",  nodename );
+		tree.put( "label", "Required Test" );
+
+		const std::string subnodename1 = "value1";
+		bpt::ptree subnode1;
+		subnode1.put( "id",  subnodename1 );
+		subnode1.put( "label", "Value 1" );
+		subnode1.put( "type", "uint8" );
+
+		const std::string subnodename2 = "value2";
+		bpt::ptree subnode2;
+		subnode2.put( "id",  subnodename2 );
+		subnode2.put( "label", "Value 2" );
+		subnode2.put( "ascii", "ok" );
+		subnode2.put( "required", "value1 + 1.5" );
+
+		tree.add_child( "group.", subnode1 );
+		tree.add_child( "group.", subnode2 );
+
+		{
+			std::ofstream stream( testFile.c_str(), std::ofstream::out );
+			stream << (uint8) 2 << "ok";
+			stream.close();
+
+			BOOST_CHECK_THROW( generateReportTree( tree ), boost::python::error_already_set );
+		}
 	}
 }
 
