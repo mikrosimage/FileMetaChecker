2375eb74a7752d96925cf3ce786a3729263b974e
Valentin Noel <van@mikrosimage.eu>
2013-07-16 15:00:39 +0200

    merging Ascii and Hexa classes into Data class

--- libraries/basicElement/src/Data/Data.cpp
+++ libraries/basicElement/src/Data/Data.cpp
@@ -1,5 +1,7 @@
 #include "Data.hpp"
 
+#include <iostream>
+
 namespace basic_element
 {
 namespace data_element
@@ -9,6 +11,7 @@ Data::Data()
 	: Element( Element::eTypeData )
 	, _data( NULL )
 	, _size( 0 )
+	, _isBigEndian( true )
 {
 }
 
@@ -21,19 +24,79 @@ Data::~Data()
 	}
 }
 
-
 void Data::setData( const char* data, const size_t& size )
 {
 	LOG_INFO( " Data: \tSET DATA " );
 	_data = new char [size];
 	_size = size;
-	std::memcpy( _data, data, _size );
+	if( !_isBigEndian )
+	{
+		std::reverse_copy( data, data + _size, _data );
+		// LOG_INFO( " Data: \t_data[0] : " << (unsigned int)_data[0] << "\t\t\t@ " << &_data );
+	}
+	else
+	{
+		std::memcpy( _data, data, _size );
+		// LOG_INFO( " Data: \t_data[0] : " << (unsigned int)_data[0] << "\t\t\t@ " << &_data );
+	}
 }
 
-char* Data::getData() const
+void Data::getData( char* buffer ) const
 {
-	LOG_INFO( " Data: \tGET DATA " );
-	return _data;
+	LOG_INFO( " Data: \tGET DATA from @ " << &_data << " to \t@ " << &buffer );
+	std::memcpy( buffer, _data, _size );
+	// LOG_INFO( " Data:\t _data : " << (unsigned int) _data[0] << "\t\t\t@ " << &_data );
+	// LOG_INFO( " Data:\t buffer: " << (unsigned int)buffer[0] << "\t\t\t@ " << &buffer );
+}
+
+size_t Data::getSize() const
+{
+	LOG_INFO( " Data: \tGET SIZE " );
+	return _size;
+}
+
+void Data::setBigEndianess( bool isBigEndian )
+{
+	_isBigEndian = isBigEndian; 
+}
+
+bool Data::getBigEndianess() const
+{
+	return _isBigEndian; 
+}
+
+std::string Data::getAscii() const
+{
+	std::stringstream sstr;
+	for (size_t i = 0; i < _size; ++i)
+		sstr << _data[i];
+	LOG_INFO( " Data: \tTO STRING (Ascii): " << sstr.str() );
+	return sstr.str();
+}
+
+std::string Data::getHexa() const
+{
+	std::stringstream sstr;
+	char* buffer = new char [ _size ];
+	std::memcpy( buffer, _data, _size );
+	for (size_t i = 0; i < _size; ++i)
+	{
+		sstr << std::hex << std::setfill('0') << std::setw(2) << (unsigned short)buffer[i];
+	}
+	delete[] buffer;
+	LOG_INFO( " Data: \tTO STRING (Hexa): " << sstr.str() );
+	return sstr.str();
+}
+
+std::vector< unsigned int > Data::toIntVector()
+{
+	std::vector< unsigned int > vector;
+	for ( size_t i = 0; i < _size; ++i )
+	{
+		vector.push_back( ( unsigned char ) _data[i] );
+		LOG_INFO( " Data: \tTO INT VECTOR : " << vector.at( i ) );
+	}
+	return vector;
 }
 
 Element::EStatus Data::checkData()
@@ -41,6 +104,20 @@ Element::EStatus Data::checkData()
 	LOG_INFO( " Data: \tCHECK DATA " );
 	return eStatusUnknown;
 }
+
+Data& Data::operator=( const Data& other )
+{
+	if( this != &other )
+	{
+		size_t size = other.getSize();
+		char* buffer = new char [ size ];
+		other.getData( buffer );
+		this->setData( buffer, size );
+		delete[] buffer;
+	}
+	return *this;
+}
+
 
 }
 }
--- libraries/basicElement/src/Data/Data.hpp
+++ libraries/basicElement/src/Data/Data.hpp
@@ -15,12 +15,26 @@ public:
 	~Data();
 
 	void setData( const char* data, const size_t& size );
-	char* getData() const;
+	void getData( char* buffer ) const;
+
+	size_t getSize() const;
+	void setBigEndianess( bool isBigEndian );
+	bool getBigEndianess() const;
+
+	std::string getAscii() const;
+	std::string getHexa() const;
+	std::vector< unsigned int > toIntVector();
+	
+
 	EStatus checkData();
+
+	Data& operator=( const Data& other );
 
 private:
 	char*  _data;
 	size_t _size;
+	bool   _isBigEndian;
+
 };
 
 }
