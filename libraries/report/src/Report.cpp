67ed4e5649723b343ce8e9b5898f2ac8498d7249
Valentin Noel <van@mikrosimage.eu>
2013-07-26 10:55:36 +0200

    add modifications in the Report class issue #30

---
+++ libraries/report/src/Report.cpp
@@ -0,0 +1,29 @@
+#include "Report.hpp"
+
+#include <common/global.hpp>
+#include <boost/foreach.hpp>
+
+namespace report_generator
+{
+
+Report::Report()
+{
+}
+
+Report::~Report()
+{
+}
+
+
+void Report::addBasicElement( std::shared_ptr< be::Element > element )
+{
+	_elementMap.insert( std::make_pair( element->getUniqueId(), element ) );
+	_basicElementTree.add( element->getId(), element->getUniqueId() );
+}
+
+std::shared_ptr< be::Element > Report::getBasicElement( const size_t uniqueId )
+{
+	return _elementMap.at( uniqueId );
+}
+
+}
--- libraries/report/src/Report.hpp
+++ libraries/report/src/Report.hpp
@@ -4,13 +4,9 @@
 #include <boost/property_tree/ptree.hpp>
 
 #include <Element.hpp>
-#include <Data/Data.hpp>
-#include <Number/Number.hpp>
 
 namespace bpt = boost::property_tree;
 namespace be  = basic_element;
-namespace bed = be::data_element;
-namespace ben = be::number_element;
 
 namespace report_generator
 {
@@ -21,29 +17,16 @@ public:
 	Report();
 	~Report();
 
-	template< typename ElementType >
-	void addBasicElement( std::shared_ptr< ElementType > element );
+	void addBasicElement( std::shared_ptr< be::Element > element );
 
 	std::shared_ptr< be::Element > getBasicElement( const size_t uniqueId );
 	
-	// void add( const std::string& entry, const std::string& value );
-	
-	// void add(const bpt::ptree &tree, const std::string& rootpath );
-
-	// void add( const std::vector< bpt::ptree >& tree, const std::string& rootpath );
-	
-	// void exportReport( const std::string& filename );
-	
 private:
 	std::map< size_t, std::shared_ptr< be::Element > > _elementMap;
-	std::map< size_t, std::shared_ptr< ben::Number > > _numberMap;	// !!!!!! Number< NumberType > !!!!!
-	std::map< size_t, std::shared_ptr< bed::Data   > > _dataMap;
 
 	bpt::basic_ptree< std::string, size_t, std::less< std::string > > _basicElementTree;
 };
 
 }
-
-#include "Report.tcc"
 
 #endif
