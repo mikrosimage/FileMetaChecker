d0c89afca5d2834fafa1a29d12397ae35e84f44d
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2013-10-05 12:07:57 +0200

    nothing

--- libraries/common/src/common/Formatters.cpp
+++ libraries/common/src/common/Formatters.cpp
@@ -8,7 +8,7 @@ boost::shared_ptr<Formatter> Formatter::get()
 	return formatter;
 }
 
-boost::shared_ptr<Formatter> Formatter::formatter(new Formatter);
+boost::shared_ptr<Formatter> Formatter::formatter( new Formatter );
 
 }
 }
