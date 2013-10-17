2ade67acbcd83d4757ed304e4dd070d17895c0e0
Valentin Noel <van@mikrosimage.eu>
2013-10-17 14:26:39 +0200

    modifications of LOG macros

--- libraries/Common/src/Common/log.hpp
+++ libraries/Common/src/Common/log.hpp
@@ -21,11 +21,11 @@ static ELogLevel level = eLogWarning;
 
 }
 
-#define LOG_TRACE( __ARGS__ )   if( common::level <= common::eLogTrace   ) std::cout << ( __ARGS__ ) << std::endl
-#define LOG_INFO( __ARGS__ )    if( common::level <= common::eLogInfo    ) std::cout << ( __ARGS__ ) << std::endl
-#define LOG_WARNING( __ARGS__ ) if( common::level <= common::eLogWarning ) std::cout << common::details::kColorYellow  << ( __ARGS__ ) << common::details::kColorStd << std::endl
-#define LOG_ERROR( __ARGS__ )   if( common::level <= common::eLogError   ) std::cout << common::details::kColorRed     << ( __ARGS__ ) << common::details::kColorStd << std::endl
-#define LOG_FATAL( __ARGS__ )   if( common::level <= common::eLogFatal   ) std::cout << common::details::kColorBoldRed << ( __ARGS__ ) << common::details::kColorStd << std::endl
+#define LOG_TRACE( ... )   if( common::level <= common::eLogTrace   ) std::cout << __VA_ARGS__ << std::endl
+#define LOG_INFO( ... )    if( common::level <= common::eLogInfo    ) std::cout << __VA_ARGS__ << std::endl
+#define LOG_WARNING( ... ) if( common::level <= common::eLogWarning ) std::cout << common::details::kColorYellow  << "Warning: " << __VA_ARGS__ << common::details::kColorStd << std::endl
+#define LOG_ERROR( ... )   if( common::level <= common::eLogError   ) std::cout << common::details::kColorRed     << "Error: "   << __VA_ARGS__ << common::details::kColorStd << std::endl
+#define LOG_FATAL( ... )   if( common::level <= common::eLogFatal   ) std::cout << common::details::kColorBoldRed << "Fatal: "   << __VA_ARGS__ << common::details::kColorStd << std::endl
 
 
 #endif
