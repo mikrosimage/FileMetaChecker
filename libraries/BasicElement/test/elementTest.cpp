0bbe7393b9e051b2da179409eae88c9ba1654d6f
Valentin Noel <van@mikrosimage.eu>
2013-10-18 12:08:33 +0200

    add Data element unit tests issue #27

---
+++ libraries/BasicElement/test/elementTest.cpp
@@ -0,0 +1,24 @@
+#define BOOST_TEST_MODULE qc_spec_reader
+
+#include <Common/Element.hpp>
+#include <Common/log.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+using namespace boost::unit_test;
+using namespace basic_element;
+
+std::string id    = "id";
+std::string label = "label";
+
+BOOST_AUTO_TEST_SUITE( basic_element_test_suite01 )
+
+BOOST_AUTO_TEST_CASE( basic_element_init )
+{
+	common::level = common::eLogTrace;
+	// LOG_INFO( common::level );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#include "dataTest.hpp"
