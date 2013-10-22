dd59322e2d0d80aa388536611ca03a4e5a182856
Valentin Noel <van@mikrosimage.eu>
2013-10-22 16:00:27 +0200

    add types to Number issue #27

--- libraries/BasicElement/src/BasicElement/SubElements/Number.hpp
+++ libraries/BasicElement/src/BasicElement/SubElements/Number.hpp
@@ -6,6 +6,17 @@
 namespace basic_element
 {
 
+typedef          char       int8;
+typedef unsigned char      uint8;
+typedef          short      int16;
+typedef unsigned short     uint16;
+typedef          int        int32;
+typedef unsigned int       uint32;
+typedef   signed long long  int64;
+typedef unsigned long long uint64;
+
+typedef long double  ieeeExtended;    // 80 bits IEEE Standard 754 floating point
+
 class Number : public Element
 {
 	
