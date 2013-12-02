b0024cc8281e27ecc9ad623b36c788a3606012bb
Valentin Noel <van@mikrosimage.eu>
2013-12-02 18:18:17 +0100

    add doc in Report.hpp issue #30

--- libraries/ReportGenerator/src/ReportGenerator/Report.hpp
+++ libraries/ReportGenerator/src/ReportGenerator/Report.hpp
@@ -14,20 +14,63 @@ namespace basic_element
 namespace report_generator
 {
 
+/**
+ *  Report generator, to print or write the comparison results.
+ */
 class Report
 {
 	typedef std::shared_ptr< basic_element::Element > ShPtrElement;
 	
 public:
+
+	/**
+	 * Report's constructor.
+	 * @param elementList Vector of Element references. Default is an empty vector.
+	 */
 	Report( const std::vector< ShPtrElement >& elementList = std::vector< ShPtrElement >() );
 	
+	/**
+	 * Add an Element to the list.
+	 * @param element Element reference to add.
+	 */
 	void add( const ShPtrElement element );
+
+	/**
+	 * Add several Elements to the list.
+	 * @param elementList Vector of Element references to add.
+	 */
 	void add( const std::vector< ShPtrElement >& elementList );
+
+	/**
+	 * Get an Element from the list.
+	 * @param elementId Element's ID.
+	 * @param iteration Element's iteration. Default is 1.
+	 * @return          Corresponding Element reference.
+	 */
 	ShPtrElement get( const std::string& elementId, const size_t& iteration = 1 );
+
+	/**
+	 * Replace an Element from the list by another with the same unique ID.
+	 * @param newElement Element reference to update.
+	 */
 	void update( const ShPtrElement newElement );
 	
+	/**
+	 * Print every printable Elements of the list.
+	 */
 	void print();
+
+	/**
+	 * Print one Element of the list.
+	 * @param element   Element reference to print.
+	 * @param dispColor Color to set to the Element display.
+	 */
 	void print( const ShPtrElement element, const std::string& dispColor );
+
+	/**
+	 * Write the Elements from the list in a XML file.
+	 * @param filename Output file name.
+	 */
 	void writeXml( const std::string& filename );
 	
 private:
